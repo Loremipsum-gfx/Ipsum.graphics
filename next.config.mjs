a6eec8b28b9e1a843eb9302fd9a39c8ad1dfce3a
Lorem Ipsum <loremipsum.private@gmail.com>
2025-10-04 21:42:00 +0200

    hero and backdrop update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // apply to anything under /graphics/*
+        source: "/graphics/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
